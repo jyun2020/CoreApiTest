05023035fb6cd7b6cd9e9fb8ed7a2301f0336a66
unknown <deliahong@uitctech.com.tw>
2021-12-17 18:35:35 +0800

    Initial commit

---
+++ ApiTest/ApiTest/Models/RegisterMemberReq.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest.Models
+{
+    public class RegisterMemberReq
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsSubscript { get; set; }
+    }
+
+    public class MemberValidator : AbstractValidator<RegisterMemberReq>
+    {
+        public MemberValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).Length(2,4);
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Age).InclusiveBetween(18, 99);
+        }
+    }
+}
---
+++ ApiTest/ApiTest/ValidateModelAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest
+{
+    public class ValidateModelAttribute : Attribute, IAsyncActionFilter
+    {
+        [ProducesResponseType(400)]
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ModelState.IsValid == false)
+            {
+                context.Result = new BadRequestObjectResult(context); // 可自製錯誤訊息回傳回去
+            }
+
+            await next();
+        }
+    }
+}
